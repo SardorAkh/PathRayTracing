afdc3587414c8739cc98b0bf7677b9f05b0a2fc9
Sardor Akhmadjonov <78022037+SardorAkh@users.noreply.github.com>
2023-02-08 23:51:49 +0500

    Upload graphics "Hello World!" :)

---
+++ PathRayTracing.cpp
@@ -0,0 +1,65 @@
+﻿// PathRayTracing.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#include <iostream>
+
+color ray_color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main()
+{
+	//Image
+
+	const auto aspect_ratio = 16.0 / 9.0;
+	int image_width = 400;
+	int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	//Camera
+
+	auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height;
+	auto focal_length = 1.0;
+
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(viewport_width, 0, 0);
+	auto vertical = vec3(0, viewport_height, 0);
+	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+
+	int color_max = 255;
+
+	std::cout << "P3\n" << " " << image_width << " " << image_height << "\n" << color_max << "\n";
+
+	for (int j = image_height - 1; j >= 0; --j) {
+		std::cerr << "\rScanLines remains: " << j << ' ' << std::flush;
+
+		for (int i = 0; i < image_width; ++i) {
+
+			auto u = double(i) / (image_width - 1);
+			auto v = double(j) / (image_height - 1);
+
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			color pixel_color = ray_color(r);
+			write_color(std::cout, pixel_color);
+		}
+	}
+	std::cerr << "\nDone";
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
